f101c68ff0e115e63195210864cb0a38781ee5a5
Sara Carolina Sanchez Arroyave <sanchez.sara2674@gmail.com>
2026-01-11 15:57:01 -0500

    Creacion de archivos para seguridad

---
+++ users/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
+
+function ProtectedRoute({ children }) {
+  const { token } = useContext(AuthContext);
+
+  if (!token) return <Navigate to="/" />;
+
+  return children;
+}
+
+export default ProtectedRoute;
